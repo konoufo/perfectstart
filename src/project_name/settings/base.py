3cad5f40341b9be99517046364b73b8c0c2db39e
konoufo <konoufo1@gmail.com>
2017-05-30 02:45:53 -0400

    trying to fix travis-ci

--- src/project_name/settings/base.py
+++ src/project_name/settings/base.py
@@ -15,7 +15,7 @@ from django.core.urlresolvers import reverse_lazy as _r
 
 # Build paths inside the project like this: join(BASE_DIR, "directory")
 BASE_DIR = abspath(dirname(dirname(dirname(__file__))))
-PROJECT_DIR = abspath(dirname(BASE_DIR))
+PROJECT_DIR = dirname(BASE_DIR)
 STATIC_ROOT = join(BASE_DIR, 'staticroot')
 STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 MEDIA_ROOT = join(BASE_DIR, 'mediaroot')
